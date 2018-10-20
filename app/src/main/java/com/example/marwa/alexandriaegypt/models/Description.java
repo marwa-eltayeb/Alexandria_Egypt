590b87212094958766fde5d8a2d4911fa30ccbed
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-10-20 19:35:24 +0200

    Travel Guide App

---
+++ app/src/main/java/com/example/marwa/alexandriaegypt/models/Description.java
@@ -0,0 +1,27 @@
+package com.example.marwa.alexandriaegypt.models;
+
+/**
+ * Created by Marwa on 10/13/2018.
+ */
+
+public class Description {
+
+    /**
+     * Description of the place
+     */
+    private String description;
+
+    /**
+     * public constructor for Description class
+     * @param description
+     */
+    public Description(String description) {
+        this.description = description;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+}
